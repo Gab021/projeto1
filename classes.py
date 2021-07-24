c5abafd8daf96e8374f19676beb6fb9576fa6532
Gab021 <gabrielnevesrj@gmail.com>
2021-07-24 20:28:38 -0300

    inclusao de widgets e ajuste da tela

--- classes.py
+++ classes.py
@@ -1,9 +1,10 @@
 class Produto:
 
-	def __init__(self,cod=0,nome='Sem nome', preco=0.0,uni='--',qtd=0):
+	def __init__(self,cod=0,nome='Sem nome',ctg='semcategoria', preco=0.0,uni='--',qtd=0):
 		self.cod = cod
 		self.nome = nome
 		self.preco =  preco
-		self.uni= uni
-		self.qtd= qtd
+		self.uni = uni
+		self.qtd = qtd
+		self.ctg = ctg
 
